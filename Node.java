8e0f1a749d646b72e4380e222269ba54533a64da
hechuno <97915928+hechuno@users.noreply.github.com>
2023-03-30 16:24:46 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Node implements Comparable<Node>{
     
-    public final String id;
-    public List<Edge> edges;
-    public double minDistance;
-    public Node previous;
+    public final String id; 
+    public List<Edge> edges;    // Liste avec tous les routes connectés au node.
+    public double minDistance;  // Distance minimale du node de départ à celui-ci.
+    public Node previous;   // Reference le node que l'algorithme de plus petit chemin a prit avant de se rendre à celui-ci.
 
     /**
      * 
@@ -13,17 +13,19 @@ public class Node implements Comparable<Node>{
      */
     public Node(String id) {
         this.id = id;
-        this.edges = new ArrayList<>();
-        this.minDistance = Integer.MAX_VALUE;
+        this.edges = new ArrayList<>(); // Initialise la liste edges comme un nouveau ArrayList.
+        this.minDistance = Integer.MAX_VALUE;   // Initialise la distance minimale du node de départ au node présent au maximum (Puisqu'on a pas encore déterminé un chemin.)
     }
 
     public void addEdge(Node to, int distance, int speed) {
-        edges.add(new Edge(to, distance, speed));
-        to.edges.add(new Edge(this, distance, speed)); // add edge in both directions
+        edges.add(new Edge(to, distance, speed));   // Ajouter une route du node A au node B.
+        to.edges.add(new Edge(this, distance, speed)); // Ajouter la même route, mais du node B au node A.
     }
 
+    //  Compare la distance minimale du node de départ à ce node avec la distance minimale du node de départ à un autre node.
+    
     public int compareTo(Node other) {
-        return Double.compare(minDistance, other.minDistance);
+        return Double.compare(minDistance, other.minDistance);  //  retourne un int négatif si plus proche du node de départ, zero si égale et positif si plus loin.
     }
     
 }
